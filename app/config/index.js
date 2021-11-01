6b678e20ebfddfecc727c08b55bf268b9001cf6e
spancavil <soancavil@gmail.com>
2021-11-01 16:20:04 -0300

    Change in .env path for work pm2 on server

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,12 @@
+import path from 'path';
 import dotenv from 'dotenv';
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, '.env')});
 
 const config = {
   server: {
