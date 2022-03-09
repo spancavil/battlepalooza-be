fc62760b7cb0a0778dd0f0f2dd8c6f5230b2279e
spancavil <soancavil@gmail.com>
2022-03-09 11:43:36 -0300

    type of request: post

--- app/routes/drops.js
+++ app/routes/drops.js
@@ -4,9 +4,9 @@ import { DropController } from '../controllers/drop.controller.js';
 const router = Router();
 
 router.post('/', DropController.getDrops);
-router.get('/detail', DropController.getDropDetail);
-router.get('/pay-coin-nft', DropController.payCoinNft);
-router.put('/buy-shop-nft', DropController.buyShopNft);
+router.post('/detail', DropController.getDropDetail);
+router.post('/pay-coin-nft', DropController.payCoinNft);
+router.post('/buy-shop-nft', DropController.buyShopNft);
 router.post('/txstatus', DropController.getTxStatus);
 
 export default router;
