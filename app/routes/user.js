b56a4aaf694646f0cdaadb0c94019a0368c63cbf
spancavil <soancavil@gmail.com>
2022-11-04 15:12:22 -0300

    Added maintenance endpoint

--- app/routes/user.js
+++ app/routes/user.js
@@ -9,6 +9,7 @@ const router = Router();
 // router.get('/search',AuthMiddleware, UserController.findAllUsers );
 // router.put('/',AuthMiddleware, UserController.updateUser );
 // router.post('/',AuthMiddleware, UserController.deleteUser );
+router.post('/check-maintenance', UserController.getMaintenanceStatus);
 router.post('/verify-recaptcha', UserController.verifyReCaptcha);
 router.post('/payload-forte', UserController.payloadForte);
 router.post('/forte-balance', UserController.forteBalance);
